6b3b36844d2d317b98549269a68b740810a27ef6
imad <j166a775433@gmail.com>
2026-09-17 16:21:48 +0100

    Build custom action with Rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'index.js',
+
+  output: {
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: false
+  },
+
+  plugins: [
+    resolve({
+      preferBuiltins: true
+    }),
+    commonjs()
+  ]
+};
